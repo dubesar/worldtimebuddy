6a9b3eeb58b08ef2803f33b4f8c7b7f916768c6b
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2024-10-06 20:06:31 +0000

    added change for installation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='worldtimebuddy',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
