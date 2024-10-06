aaee42277827ac8a01a2eea3bc55875c7f6c1276
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2024-10-06 23:10:56 +0530

    Added improvements for fetching values from dictionary (#8)
    * Improvement in using the environment variables
    
    * Added change in the Readme

--- worldtimebuddy/utils.py
+++ worldtimebuddy/utils.py
@@ -3,7 +3,7 @@ from typing import List
 
 def get_major_tz_from_env() -> List[str]:
     try:
-        major_time_zones = os.environ["MAJOR_TIMEZONES"]
+        major_time_zones = os.environ.get("MAJOR_TIMEZONES", None)
         return list(major_time_zones[1:len(major_time_zones) - 1].split(','))
     except:
         return []
